26734b27f8d551608012fb1260fb26ab5dedf9d2
Fonta97 <fontana.alessandro97@gmail.com>
2024-01-14 15:53:18 +0100

    Bonus: implemented and base code finished

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,99 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+// importo axios
+import axios from 'axios';
+
+// importo componenti
+import AppHeader from './components/AppHeader.vue'
+import ListItems from './components/ListItems.vue'
+import AppSearch from './components/AppSearch.vue'
+import AppResult from './components/AppResult.vue'
+
+// importo store
+import { store } from './store';
+
+
+export default {
+  components: {
+    AppHeader,
+    ListItems,
+    AppSearch,
+    AppResult
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    getCard() {
+      let myURL = store.apiURL;
+      let archURL = store.optionApiURL;
+
+      if (store.searchText !== '') {
+        if (myURL.includes('?')) {
+          // se l'URL già contiene ?, aggiungo & e il nuovo parametro
+          myURL += `&${store.archParam}=${store.searchText}`;
+        } else {
+          // Se l'URL non contiene ancora parametri, aggiungi ? seguito dal nuovo parametro
+          myURL += `?${store.archParam}=${store.searchText}`;
+        }
+
+      }
+
+      // chiamata api cards
+      axios
+        .get(myURL)
+        .then((res => {
+          // console.log(res.data.data);
+          store.cardList = res.data.data;
+        }))
+        .catch((err) => {
+          console.log("errori", err);
+        })
+
+      // chiamata api archetype
+      axios
+        .get(archURL)
+        .then((res => {
+          // console.log(res.data);
+          store.archList = res.data;
+        }))
+        .catch((err) => {
+          console.log("errori", err);
+        })
+    }
+  },
+  created() {
+    this.getCard();
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader />
+  <main>
+    <div class="container">
+      <AppSearch @filter="getCard" />
+      <AppResult />
+      <ListItems />
+    </div>
+
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+
+main {
+  padding: 120px 0 50px;
+  background-color: $main-color;
+
+  .container {
+    width: 80%;
+    margin: 0 auto;
+    background-color: white;
+    padding: 20px;
+  }
 }
 </style>
---
+++ src/components/ListItems.vue
@@ -0,0 +1,43 @@
+<script >
+import SingleItem from '../components/SingleItem.vue'
+
+
+// importo store
+import { store } from '../store';
+
+export default {
+    name: 'ListItems',
+    components: {
+        SingleItem,
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="row">
+        <a href="#" class="card" v-for="card in store.cardList" :key="card.id">
+            <SingleItem :info="card" />
+        </a>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+.row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+    .card {
+        flex-basis: calc((100% / 5) - 20px);
+        margin: 10px;
+        background-color: $main-color;
+    }
+}
+</style>
